632cfa17c562531b453eeb0513c182bdc7927ee0
Eric V. Strobl <ericvonstrobl@gmail.com>
2024-12-07 09:27:50 -0500

    Update posthoc_test.R

--- R/posthoc_test.R
+++ R/posthoc_test.R
@@ -1,28 +1,56 @@
-posthoc_test <- function(mod,Tx,Y,nE,nperms=10000){
+posthoc_test <- function(mod,Tx,Y,nperms=10000){
   # post-hoc permutation testing
+  # Inputs:
   #   mod = original model from SV function
   #   Tx = vector of treatments for n samples
   #   Y = n by p matrix of p rating scale items
-  #   nE = number of components/factors ELIMINATED after diagnostics
+  #
+  # Outputs:
+  #   p = uncorrected p-values for each treatment pair and factor
+  #   pFWER = FWER corrected p-values across treatments
+  #   stat = absolute difference statistic
   #
   #
   # written by Eric V. Strobl, 12/3/2024
   
   require(qvalue)
   
-  pval = 0
-  stat = (apply(mod$MR,2,max) - apply(mod$MR,2,min))
+  pval = matrix(0,(nt^2-nt)/2,ncol(mod$MR)) # uncorrected p-values
+  pFWER = matrix(0,(nt^2-nt)/2,ncol(mod$MR))
   n = nrow(Y)
+  
+  # compute absolute difference statistic for original samples
+  abs_diff = matrix(0,(nt^2-nt)/2,ncol(mod$MR))
+  for (j in 1:ncol(mod$MR)){
+    abs_diff[,j] = as.numeric(dist(mod$MR[,j]))
+  }
+  
+  # perform permutation statistic
   for (p in 1:nperms){
     cat('\r',p)
     perm = sample(1:n,n,replace=FALSE)
     mod1 = SV(Tx,Y[perm,],ee = mod$eigen)
-    pval = pval + (stat <= (apply(mod1$MR,2,max) - apply(mod1$MR,2,min)))
+    
+    range = apply(mod1$MR,2,max) - apply(mod1$MR,2,min) # range statistic for FWER
+    for (j in 1:ncol(mod$MR)){
+      pFWER[,j] = pFWER[,j] + (abs_diff[,j] < range[j])
+      pval[,j] = pval[,j] + (abs_diff[,j] < as.numeric(dist(mod1$MR[,j])))
+    }
   }
+  pval = pval/nperms
+  pFWER = pFWER/nperms
   
-  ps = pval/nperms
-  d = length(ps)
-  qs = qvalue(ps,pi0=(d-nE-1)/d)$qvalues
+  # label comparisons
+  labels = c()
+  tx = unique(Tx)
+  for (c in 1:(length(tx)-1)){
+    for (d in (c+1):length(tx)){
+      labels = c(labels, paste(as.character(tx[c]), "-", as.character(tx[d]),sep=""))
+    }
+  }
+  rownames(pval) = labels
+  rownames(pFWER) = labels
+  rownames(abs_diff) = labels
   
-  return( list(p = pval/nperms, p_FDR = qs, stat =  stat) )
+  return( list(p = pval, pFWER = pFWER, stat =  abs_diff) )
 }
